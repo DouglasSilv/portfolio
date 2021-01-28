07873115ef01158bb6bc8ff3f18e7994e7ac9177
Douglas <douglas.silva@dbccompany.com.br>
2021-01-27 21:40:19 -0300

    refactor _document.js

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,4 @@
 import { GlobalStyle } from '../config/GlobalStyle';
-import Head from 'next/head';
 import Header from '../components/Header';
 import Home from '../components/Home';
 import SocialNetworks from '../components/SocialNetworks';
@@ -8,13 +7,6 @@ import Sections from '../components/Sections';
 const Main = () => {
   return (
     <>
-      <link
-        href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-        rel="stylesheet"
-      ></link>
-      <Head>
-        <title>Douglas Silva - Software Developer</title>
-      </Head>
       <GlobalStyle />
       <Header />
       <SocialNetworks />
